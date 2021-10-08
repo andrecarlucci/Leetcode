47669da4d98b4b46b3369125ebbc6933a86230fc
andrecarlucci <andrecarlucci@gmail.com>
2021-10-08 18:33:32 +0200

    LongestSubstringWithoutRepeatingCharacters

---
+++ Leetcode.Tests/LongestSubstringWithoutRepeatingCharacters.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Leetcode.Tests
+{
+    public class LongestSubstringWithoutRepeatingCharacters
+    {
+        public int LengthOfLongestSubstring(string s)
+        {
+            if (s.Length == 0)
+            {
+                return 0;
+            }
+            var map = new Dictionary<char, int>();
+            int max = 0;
+            var j = 0;
+            for (var i = 0; i < s.Length; ++i)
+            {
+                if (map.ContainsKey(s[i]))
+                {
+                    j = Math.Max(j, map[s[i]] + 1);
+                }
+                map[s[i]] = i;
+                max = Math.Max(max, i - j + 1);
+            }
+            return max;
+        }
+
+        [Theory]
+        [InlineData("abcabcbb", 3)]
+        [InlineData("bbbbb",1)]
+        [InlineData("pwwkew", 3)]
+        [InlineData("", 0)]
+        [InlineData(" ", 1)]
+        [InlineData("au", 2)]
+        [InlineData("dvdf", 3)]
+        [InlineData("anviaj", 5)]
+        [InlineData("asdfea", 5)]
+        [InlineData("asdfasdf", 4)]
+        public void LengthOfLongestSubstringTest(string w, int result)
+        {
+            Assert.Equal(result, LengthOfLongestSubstring(w));
+        }
+    }
+}
